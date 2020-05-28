0fcf8c2161339d454ce4469206f39c1f778bac3a
daddington <daniel.addington@trademe.co.nz>
2020-05-28 16:29:03 +1200

    extract dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havenese" />
-      <Pet name="Duffy" animal="Bird" breed="Parrot" />
+      <SearchParams />
+      {/* <Pet name="Luna" animal="Dog" breed="Havenese" />
+      <Pet name="Duffy" animal="Bird" breed="Parrot" /> */}
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breeds", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed]);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label>
+          {location}
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
+          ></input>
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
